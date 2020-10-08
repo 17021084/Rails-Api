0bf078e62bce01d466183b34ec5fef802fd5047c
17021084 <dotrunghg1999@gmail.com>
2020-10-08 22:07:34 +0700

    Use mailstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,14 +54,25 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name: 'dotrunghg111999@gmail.com',
+  #   password: '02192210246',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
+
+
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: 'dotrunghg111999@gmail.com',
-    password: '02192210246',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+config.action_mailer.smtp_settings = {
+  :user_name => ENV['MAILTRAP_USERNAME'],
+  :password =>  ENV['MAILTRAP_PASSWORD'],
+  :address => 'smtp.mailtrap.io',
+  :domain => 'smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
 end
